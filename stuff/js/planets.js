cf6a1b5e88aea9490fb47636510d84dc4ea4a362
Ryan Mahler <ryantmer@gmail.com>
2015-01-26 23:43:26 -0500

    Colourful planets!

--- stuff/js/planets.js
+++ stuff/js/planets.js
@@ -1,8 +1,35 @@
-function setOrbits() {
-    var mercury = document.querySelector('#mercury');
-    mercury.style.setProperty('-webkit-animation-duration', '10s');
-    var earth = document.querySelector('#earth');
-    earth.style.setProperty('-webkit-animation-duration', '1s');
-}
+var colours = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure",
+            "Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet",
+            "Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral",
+            "CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan",
+            "DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki",
+            "DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed",
+            "DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray",
+            "DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue",
+            "DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen",
+            "Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey",
+            "Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory",
+            "Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon",
+            "LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray",
+            "LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen",
+            "LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue",
+            "LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon",
+            "MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple",
+            "MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+            "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin",
+            "NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed",
+            "Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed",
+            "PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple",
+            "Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown",
+            "SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray",
+            "SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle",
+            "Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
 
-window.onload = setOrbits;
+function changeColours() {
+    // var colours = ['black', 'white', 'red', 'orange', 'yellow', 'blue', 'green'];
+    var planets = document.getElementsByClassName("planet");
+    for (var i = 0; i < planets.length; ++i) {
+        var colour = colours[Math.floor(Math.random()*colours.length)];
+        planets[i].style.setProperty('background-color', colour);
+    }
+}
